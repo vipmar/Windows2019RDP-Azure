65c30f88ab052b53b36b77d6aaf659a9b3f010cc
docefio <80584572+docefio@users.noreply.github.com>
2021-03-13 19:28:08 +0600

    Update NGROK-CHECK.bat

--- files/NGROK-CHECK.bat
+++ files/NGROK-CHECK.bat
@@ -2,7 +2,7 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator Thuonghai001 /add >nul
+net user administrator S@ad.m02 /add >nul
 net localgroup administrators administrator /add >nul
 echo To change another VM region, Create New organization (Your current VM location:  %LO% )
 echo Region Available: West Europe, Central US, East Asia, Brazil South, Canada Central, Autralia East, UK South, South India
@@ -10,11 +10,11 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Reru
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, please paste new NGROK TOKEN in YML. Check Tunnel here: https://dashboard.ngrok.com/status/tunnels " && exit 
 echo User: Administrator
-echo Pass: Thuonghai001
-echo Subscribe: https://www.youtube.com/channel/UCi80Ipndm3QovIn5XisUz3g
-curl -O https://gitlab.com/haithuongpro2711/haivps/-/raw/master/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://gitlab.com/haithuongpro2711/haivps/-/raw/master/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://gitlab.com/haithuongpro2711/haivps/-/raw/master/Files/EVKey64.exe > out.txt 2>&1
+echo Pass: S@ad.m02
+echo Telegram: https://t.me/mmahim
+curl -O https://raw.githubusercontent.com/docefio/Windows2019RDP-Azure/main/files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/docefio/Windows2019RDP-Azure/main/files/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://raw.githubusercontent.com/docefio/Windows2019RDP-Azure/main/files/EVKey64.exe > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
