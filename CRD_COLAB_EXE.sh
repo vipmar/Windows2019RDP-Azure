016347d28ddc379074b93ef6a2ef6d1463d2f407
docefio <80584572+docefio@users.noreply.github.com>
2021-03-13 20:09:13 +0600

    Update CRD_COLAB_EXE.sh

--- CRD_COLAB_EXE.sh
+++ CRD_COLAB_EXE.sh
@@ -20,7 +20,7 @@ printf """$c$b
     | |    / _ \| |/ _\` | '_ \    |  __  |/ _\` |/ __| |/ / __|
     | |___| (_) | | (_| | |_) |   | |  | | (_| | (__|   <\__ \\
      \_____\___/|_|\__,_|_.__/    |_|  |_|\__,_|\___|_|\_\___/ 
-    $r  By Pradyumna Krishna © 2020 $c Edited by https://fb.com/thuong.hai.581 (v5 fix terminal issue)
+    $r  By Pradyumna Krishna © 2020 $c Edited by https://t.me/mmahim (v5 fix terminal issue)
       Github : https://github.com/PradyumnaKrishna/Colab-Hacks          
 $endc$enda""";
 
